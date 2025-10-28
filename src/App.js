d9e0c6e560091fd79ad9f299b410501f91a7c3e7
jabss <jabrijuhinin93@gmail.com>
2025-10-28 21:34:13 +0800

    create components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import Header from './components/Header';
+import AddContent from './components/AddContact';
+import ContactList from './components/ContactList';
+import AddContact from './components/AddContact';
 
 function App() {
   return (
-    <div> Hello World !</div>
+    <div className='ui container'> 
+      <Header/>
+      <AddContact/>
+      <ContactList/>
+      {/* <ContactCard/> */}
+    </div>
   )
 }
 
