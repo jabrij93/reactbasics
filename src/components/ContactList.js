89acdd1d3fd67de976bf6de676c4b5d7a832b13b
jabss <jabrijuhinin93@gmail.com>
2025-10-29 16:15:03 +0800

    contact manager part1 assignment completed(polished ui ux)

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -2,22 +2,35 @@ import React from 'react'
 import ContactCard from './ContactCard'
 
 const ContactList = (props) => {
+  const deleteContactHandler = (id) => {
+    props.getContactId(id)
+  }
 
-    const deleteContactHandler = (id) => {
-        props.getContactId(id);
-    }
-    
-    const renderContactList = props.contacts.map((contact) => {
-        return (
-            <ContactCard contact={contact} clickHandler={deleteContactHandler} />
-        )
-    })
-
+  const renderContactList = props.contacts.map((contact) => {
     return (
-        <div className='ui celled list'>
-            {renderContactList}
-        </div>
+      <ContactCard
+        contact={contact}
+        clickHandler={deleteContactHandler}
+        key={contact.id}
+      />
     )
+  })
+
+  return (
+    <div className='ui celled list'>
+      <h2 style={{ textAlign: 'left', marginBottom: '15px', color: '#333' }}>
+        Contacts
+      </h2>
+
+      {renderContactList.length > 0 ? (
+        renderContactList
+      ) : (
+        <p style={{ color: '#777', marginLeft: '5px' }}>
+          No contacts available. Add a new contact above.
+        </p>
+      )}
+    </div>
+  )
 }
 
 export default ContactList
