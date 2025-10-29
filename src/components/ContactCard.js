2172927362873c069c2997ef28cbef7b57342cd7
jabss <jabrijuhinin93@gmail.com>
2025-10-29 16:04:32 +0800

    create ContactCard.css

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -1,34 +1,26 @@
 import React from 'react'
+import './ContactCard.css'
 import user from '../images/user.png'
 
 const ContactCard = (props) => {
-    const {id, name, email} = props.contact;
-    return (
-        <div className='item' style={{padding: '10px'}}>
-            <img 
-                className='ui avatar image' 
-                src={user} 
-                alt='user'
-            />
-            <div className='content'>
-                <div className='header'>{name}</div>
-                <div>{email}</div>
-            </div>
-            <i 
-                className='trash alternate outline icon' 
-                style={{
-                    color: "red",
-                    marginTop: "7px",
-                    cursor: "pointer",
-                    transition: "transform 0.2s ease",
-                  }}
-                onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.2)")}
-                onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
-                onClick={ () => props.clickHandler(id) }
-            >
-            </i>
+  const { id, name, email } = props.contact
+
+  return (
+    <div className='contact-item'>
+      <div className='contact-info'>
+        <img className='ui avatar image' src={user} alt='user' />
+        <div className='content'>
+          <div className='header'>{name}</div>
+          <div>{email}</div>
         </div>
-    )
+      </div>
+
+      <i
+        className='trash alternate outline icon delete-icon'
+        onClick={() => props.clickHandler(id)}
+      ></i>
+    </div>
+  )
 }
 
-export default ContactCard;
+export default ContactCard
