a08a04ac82b743e00b23f34208e2db218d7dce66
jabss <jabrijuhinin93@gmail.com>
2025-12-18 16:08:07 +0800

    data is now fetched from json-server(db.json)

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
-import './App.css';
 import Header from './components/Header';
 import AddContact from './components/AddContact';
 import ContactList from './components/ContactList';
 import PopupMessage from './components/PopupMessage';
 import ContactDetail from './components/ContactDetail';
+import api from '../src/api/contacts';
+import './App.css';
+
 
 function App() {
   const LOCAL_STORAGE_KEY = "contacts";
-  const [contacts, setContacts] = useState(() => {
-    const storedContacts = localStorage.getItem(LOCAL_STORAGE_KEY);
-    return storedContacts ? JSON.parse(storedContacts) : [];
-  });  
+
+  const [contacts, setContacts] = useState([]);  
   const [popup, setPopup] = useState({ show: false, message: '', type: 'success' });
 
   const addContactHandler = (contact) => {
@@ -33,10 +33,24 @@ function App() {
     setTimeout(() => setPopup({ show: false, message: '', type }), 2000);
   };
 
+  // Retrieve Contacts 
+
+  const retrieveContacts = async () => {
+    const response = await api.get("/contacts");
+    console.log("API response:", response);
+    console.log("API data:", response.data);
+    return response.data;
+  };
+
   // Load saved contacts from localStorage on app load
   useEffect(() => {
-    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (retrieveContacts) setContacts(retrieveContacts);
+    const getAllContacts = async () => {
+      const allContacts = await retrieveContacts();
+      if (allContacts) {
+        setContacts(allContacts);
+      }
+    }
+    getAllContacts();
   }, []);
 
   // Save contacts to localStorage whenever they change
